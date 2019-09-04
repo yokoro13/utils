fe2131fce51a681edf7c9434a16d67d301a322ab
yokoro13 <34597246+yokoro13@users.noreply.github.com>
2019-09-04 14:14:22 +0900

    Update ADE20Kto8Classes.py

--- ADE20Kto8Classes.py
+++ ADE20Kto8Classes.py
@@ -2,15 +2,15 @@ import numpy as np
 from PIL import Image
 import os
 from tqdm import tqdm
-import cv2
 
 ade_path = '../../Documents/ADEChallengeData2016/annotations/'
+target = "validation"   # train or validation
 
 an_path = os.listdir(ade_path)
 
 img_dir = os.listdir(ade_path + "validation")
 for img_path in tqdm(img_dir):
-    img_path_ = os.path.join('../../Documents/ADEChallengeData2016/annotations/validation', img_path)
+    img_path_ = os.path.join(ade_path + target, img_path)
     img = Image.open(img_path_)
     img = np.asarray(img)
     img = np.where((img != 1) & (img != 2), 0, 1) + \
@@ -22,13 +22,4 @@ for img_path in tqdm(img_dir):
           np.where(img != 25, 0, 7)
 
     img = Image.fromarray(img)
-    img.save('../../Documents/ADEChallengeData2016/annotations/validation' + "5c/" + img_path)
-
-"""
-img = Image.open("./1.png")
-img = np.asarray(img)
-print(img)
-
-print(img)
-img = Image.fromarray(img)
-img.save("4.png")"""
+    img.save(ade_path + target + "8c/" + img_path)
